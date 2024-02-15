861f05abc92a56c574d0436162ade2e5694e55d3
Sahil Malavi <110047193+SahilMalavi@users.noreply.github.com>
2024-02-15 17:10:07 +0530

    Update RudraAI.py

--- RudraAI.py
+++ RudraAI.py
@@ -55,115 +55,117 @@ create_chat()
 #             return command.lower()
 #     except:
 #         return ""
-
-def rudra(query):
-    try:
-        # query = speechRecognition()
-        print("\n ==> Master : ",query)
-
-       
-        if 'current time' in query or 'todays date' in query:
-                if 'time' in query and 'date' in query:
-                    time = datetime.datetime.now().strftime('%I:%M %p')
-                    current_date = datetime.datetime.now().date()
-                    return 'Current time is ' + time +' and '+'Todays date is '+str(current_date)
-                elif 'time' in query:
-                    time = datetime.datetime.now().strftime('%I:%M %p')
-                    return 'Current time is ' + time
-                elif 'date' in query:
-                    current_date = datetime.datetime.now().date()
-                    return 'Todays date is '+str(current_date)
-                
- 
-        elif 'open website' in query:
-            if ".com" in query or ".in" in query or ".org" in query or ".online" in query or ".io" in query:
-                openweb = query.replace("open", "")
-                openweb= openweb.replace("website","")
-                webbrowser.open(openweb)
-                return "opening "+openweb
-            else:
-                return "please say command like this. (example-open website youtube.com )"
-
-        # elif 'play on youtube' in query:
-        #         song = query.replace("play", "")
-        #         song = song.replace("on youtube", "")
-        #         pywhatkit.playonyt(song)
-        #         return "playing."
-
-        # elif "bye" in query:
-        #     sys.exit()
-        #     return speak("Nice to meet you Master, Bye")
+try:
+    def rudra(query):
+        try:
+            # query = speechRecognition()
+            print("\n ==> Master : ",query)
+    
            
-
-        elif not query:
-             return "please say command again.."
-
-        else:
-            # prompt=f" pls respond to: {query}"  
-            # print(prompt)
-            response = gemini_response(query)
-            print()
-            return response
-
-    except:
-         return "Error is occured while execution.!"
-         
-         
+            if 'current time' in query or 'todays date' in query:
+                    if 'time' in query and 'date' in query:
+                        time = datetime.datetime.now().strftime('%I:%M %p')
+                        current_date = datetime.datetime.now().date()
+                        return 'Current time is ' + time +' and '+'Todays date is '+str(current_date)
+                    elif 'time' in query:
+                        time = datetime.datetime.now().strftime('%I:%M %p')
+                        return 'Current time is ' + time
+                    elif 'date' in query:
+                        current_date = datetime.datetime.now().date()
+                        return 'Todays date is '+str(current_date)
+                    
+     
+            elif 'open website' in query:
+                if ".com" in query or ".in" in query or ".org" in query or ".online" in query or ".io" in query:
+                    openweb = query.replace("open", "")
+                    openweb= openweb.replace("website","")
+                    webbrowser.open(openweb)
+                    return "opening "+openweb
+                else:
+                    return "please say command like this. (example-open website youtube.com )"
     
-def main():
-    st.title("Rudra AI")
-
-    st.sidebar.title("ASK TO IMAGE")
-    uploaded_image = st.sidebar.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-    # ask_to_image_button = st.sidebar.button("Upload")
-
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    prompt="hey Rudra, i am Sahil, your task is to serve my query's, or talk with me, [instruction for you purpose only: please sometime answer in only single line and sometime answer briefly point wise according to the query,in english only, you can also able to open any webpages on the internet, you only tell user to enter [open website youtube.com] this command. only tell user to enter [what is current time] this command to know current time,only tell user to enter [what is todays date] this command, to know todays date],okay so hii rudra"
+            # elif 'play on youtube' in query:
+            #         song = query.replace("play", "")
+            #         song = song.replace("on youtube", "")
+            #         pywhatkit.playonyt(song)
+            #         return "playing."
     
-    first_response=gemini_response(prompt)
-
-    # st.session_state.messages.append({"role": "assistant", "content": first_response})
-    with st.chat_message('assistant'):
-            st.markdown(first_response)
-
-    # st.session_state.messages.clear()
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        st.image(image, caption="Uploaded Image")
-        st.sidebar.write("remove image to go back to the Rudra AI")
+            # elif "bye" in query:
+            #     sys.exit()
+            #     return speak("Nice to meet you Master, Bye")
+               
+    
+            elif not query:
+                 return "please say command again.."
+    
+            else:
+                # prompt=f" pls respond to: {query}"  
+                # print(prompt)
+                response = gemini_response(query)
+                print()
+                return response
+    
+        except:
+             return "Error is occured while execution.!"
+             
+             
         
-        if prompt := st.chat_input("Ask to image"):
-            prompt= prompt.lower()
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            with st.chat_message("user"):
-                st.markdown(prompt)
-            with st.chat_message("assistant"):
-                message_placeholder = st.empty()
-                response = gemini_IMGresponse(prompt, image)
-                message_placeholder.markdown(response)
-                print("\n ==> Rudra Image AI :", response)
-
-            st.session_state.messages.append({"role": "assistant", "content": response})
-    else:
-        if prompt := st.chat_input("Ask Rudra"):
-            prompt= prompt.lower()
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            with st.chat_message("user"):
-                st.markdown(prompt)
-            with st.chat_message("assistant"):
-                message_placeholder = st.empty()
-                response = rudra(prompt)
-                message_placeholder.markdown(response)
-                print("\n ==> Rudra AI :", response)
-            st.session_state.messages.append({"role": "assistant", "content": response})
-
+    def main():
+        st.title("Rudra AI")
+    
+        st.sidebar.title("ASK TO IMAGE")
+        uploaded_image = st.sidebar.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+        # ask_to_image_button = st.sidebar.button("Upload")
+    
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+    
+        prompt="hey Rudra, i am Sahil, your task is to serve my query's, or talk with me, [instruction for you purpose only: please sometime answer in only single line and sometime answer briefly point wise according to the query,in english only, you can also able to open any webpages on the internet, you only tell user to enter [open website youtube.com] this command. only tell user to enter [what is current time] this command to know current time,only tell user to enter [what is todays date] this command, to know todays date],okay so hii rudra"
+        
+        first_response=gemini_response(prompt)
+    
+        # st.session_state.messages.append({"role": "assistant", "content": first_response})
+        with st.chat_message('assistant'):
+                st.markdown(first_response)
+    
+        # st.session_state.messages.clear()
+    
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+    
+        if uploaded_image is not None:
+            image = Image.open(uploaded_image)
+            st.image(image, caption="Uploaded Image")
+            st.sidebar.write("remove image to go back to the Rudra AI")
+            
+            if prompt := st.chat_input("Ask to image"):
+                prompt= prompt.lower()
+                st.session_state.messages.append({"role": "user", "content": prompt})
+                with st.chat_message("user"):
+                    st.markdown(prompt)
+                with st.chat_message("assistant"):
+                    message_placeholder = st.empty()
+                    response = gemini_IMGresponse(prompt, image)
+                    message_placeholder.markdown(response)
+                    print("\n ==> Rudra Image AI :", response)
+    
+                st.session_state.messages.append({"role": "assistant", "content": response})
+        else:
+            if prompt := st.chat_input("Ask Rudra"):
+                prompt= prompt.lower()
+                st.session_state.messages.append({"role": "user", "content": prompt})
+                with st.chat_message("user"):
+                    st.markdown(prompt)
+                with st.chat_message("assistant"):
+                    message_placeholder = st.empty()
+                    response = rudra(prompt)
+                    message_placeholder.markdown(response)
+                    print("\n ==> Rudra AI :", response)
+                st.session_state.messages.append({"role": "assistant", "content": response})
+except:
+    st.write("We apologize for the inconvenience. Please check back in a few minutes.");
+    
 if __name__ == "__main__":
     main()
 
