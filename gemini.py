342e1f347702b2ed988238611bcc275d5a7970f4
softclones <sahilmalavi96@gmail.com>
2025-10-05 01:26:26 +0530

    commiting with changes

--- gemini.py
+++ gemini.py
@@ -14,10 +14,10 @@ def to_markdown(text):
     text = text.replace('•', '  *')
     return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-ai.configure(api_key="AIzaSyCvhpNhCsZkxnR2bie-RM6U5EcPGJYsYbU")
+ai.configure(api_key="AIzaSyAP8M_8zf_Ks-KryXGSsXSEZlyL8wDocy0")
 
 def create_chat():
-    model = ai.GenerativeModel('gemini-2.5-flash')
+    model = ai.GenerativeModel('gemini-2.0-flash')
     chat = model.start_chat(history=[])
     return chat
 
@@ -28,11 +28,7 @@ def gemini_response(input):
     return response.text 
 
 def gemini_IMGresponse(prompt, img):
-    model = ai.GenerativeModel('gemini-1.5-flash')
+    model = ai.GenerativeModel('gemini-2.5-flash')
     response = model.generate_content([prompt, img], stream=True)
     response.resolve()
     return response.text
-
-    
-
-
