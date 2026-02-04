16b156ec2543f863b914c833607fe6e3e2cc378a
Sahil Malavi <sahilmalavi96@gmail.com>
2026-02-05 01:20:38 +0530

    frontend url added to backend origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
-
+        "https://rudra-ai-bot.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
